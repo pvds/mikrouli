e1b267aeba7f4c83199efb6f039ff074902def0a
pvdsteen <pvdsteen@gmail.com>
2024-12-23 13:31:09 +0100

    feat: add layout template and import app css

---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+
+	/** @type {{children: import('svelte').Snippet}} */
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<main>
+		{@render children()}
+	</main>
+</div>
