0dad9b9bea76dc6ac5a27a5b390d719f1131769d
pvdsteen <pvdsteen@gmail.com>
2024-12-23 19:53:25 +0100

    feat: use ssg adapter instead of netlify

---
+++ site/src/routes/+layout.js
@@ -0,0 +1,2 @@
+// Prerender all pages because we're not using a fallback (i.e. SPA mode)
+export const prerender = true;
---
+++ site/src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About Mikrouli" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About Mikrouli</h1>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+</div>
