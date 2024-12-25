f858a6ad71a83a1e2d572f322b8e6ee5413e55dc
pvdsteen <pvdsteen@gmail.com>
2024-12-25 23:24:55 +0100

    fix: tailwindcss auto-completion
    Importing from vitest/config breaks tailwindcss plugin auto-completion.

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,6 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
+// TODO: importing from vitest/config breaks tailwindcss plugin auto-completion
+// import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
