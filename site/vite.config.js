e40112b710474d36c04f70c3032b7289eec3386a
pvdsteen <pvdsteen@gmail.com>
2024-12-26 22:48:08 +0100

    fix: set base url directly in defineConfig

--- site/vite.config.js
+++ site/vite.config.js
@@ -3,10 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [
-		sveltekit({
-			base: "./",
-		}),
-		tailwindcss(),
-	],
+	plugins: [sveltekit(), tailwindcss()],
+	base: "./",
 });
