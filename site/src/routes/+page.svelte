1915fc719afc6d960427f4bcaaa3a161807fb541
pvdsteen <pvdsteen@gmail.com>
2024-12-26 13:51:11 +0100

    fix: home page content

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="italic">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import Hero from "$lib/components/shell/Hero.svelte";
+</script>
+
+<Hero title="A Platform for systemic change">
+	<p>
+		We are a platform for systemic change.
+		We offer a range of services to help you with your mental health and well-being.
+		Systemic therapy, peer support, and advocacy are some of the ways we can support you.
+	</p>
+</Hero>
