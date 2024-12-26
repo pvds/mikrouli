cd52992bde197a12b6a939aa4584ae4ceb2ef206
pvdsteen <pvdsteen@gmail.com>
2024-12-26 11:29:30 +0100

    fix: set relative base path

--- site/vite.config.js
+++ site/vite.config.js
@@ -3,5 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
+	plugins: [
+		sveltekit({
+			base: "./",
+		}),
+		tailwindcss(),
+	],
 });
