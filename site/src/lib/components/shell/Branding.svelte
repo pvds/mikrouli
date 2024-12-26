cac177b4a70ee8e385b249c686b982322303573e
pvdsteen <pvdsteen@gmail.com>
2024-12-26 13:47:57 +0100

    fix: home link

--- site/src/lib/components/shell/Branding.svelte
+++ site/src/lib/components/shell/Branding.svelte
@@ -5,6 +5,6 @@ import Logo from "./Logo.svelte";
 <section class="flex gap-3 items-center">
 	<Logo classes="fill-accent-200 size-7"/>
 	<h1 class="font-medium text-2xl">
-		<a href="/">Mikrouli</a>
+		<a href="./">Mikrouli</a>
 	</h1>
 </section>
