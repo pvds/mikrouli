178ddaa5773d9d27a9e58bf43480b8fbab2ac30b
pvdsteen <pvdsteen@gmail.com>
2024-12-28 11:35:26 +0100

    fix: remove duplicate content

--- src/lib/components/shell/Hero.svelte
+++ src/lib/components/shell/Hero.svelte
@@ -17,11 +17,3 @@ let { title, children } = $props();
 		{@render children?.()}
 	</div>
 </Section>
-
-
-<Section>
-	<h1 class="text-4xl mb-4 font-extrabold">{title}</h1>
-	<div class="prose-lg italic">
-		{@render children?.()}
-	</div>
-</Section>
