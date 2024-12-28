47b721ed98055ea6796c54d8f908bb8a35cdc035
pvdsteen <pvdsteen@gmail.com>
2024-12-28 04:04:32 +0100

    fix: layout typing and error handling

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,20 +1,15 @@
+import { error } from "@sveltejs/kit";
+
 export const prerender = true; // Ensure the site is statically generated
 
-/** @type {import('./$types').PageLoad} */
+/** @type {import('./$types').LayoutLoad} */
 export async function load({ fetch, params }) {
 	try {
 		const res = await fetch("./siteData.json");
-
-		// Check if the response status is ok (status code 200–299)
-		if (!res.ok) {
-			throw new Error(
-				`Failed to fetch data: ${res.status} ${res.statusText}`,
-			);
-		}
+		if (!res.ok) error(res.status, res.statusText);
 
 		/** @type {import('$lib/types/contentful').ContentfulData} */
 		const data = await res.json();
-
 		return {
 			navigation: data.navigation,
 			pages: data.pages,
@@ -23,8 +18,8 @@ export async function load({ fetch, params }) {
 		console.error("Error loading data:", error);
 
 		return {
-			error: true,
-			message: error.message,
+			navigation: [],
+			pages: [],
 		};
 	}
 }
