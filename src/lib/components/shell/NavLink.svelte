88f62b21cd3e7c316ff4f310ef43cbe0a0640030
pvdsteen <pvdsteen@gmail.com>
2024-12-28 13:01:15 +0100

    feat: add current navlink classes

--- src/lib/components/shell/NavLink.svelte
+++ src/lib/components/shell/NavLink.svelte
@@ -9,12 +9,12 @@
 /** @type {Props} */
 let { href, currentPath, children } = $props();
 
-let isCurrentPage = $derived(currentPath === href);
+let isCurrentPage = $derived(currentPath.includes(href));
 </script>
 
 <li>
 	<a {href}
-	class="inline-flex px-3 py-1 rounded-lg font-medium hover:text-accent-400"
+	class="inline-flex px-4 py-1 rounded-3xl font-medium hover:text-accent-400 {isCurrentPage ? 'bg-brand-900 hover:text-brand-50' : null}"
 	aria-current={isCurrentPage ? "page" : undefined}
 	>
 		{@render children?.()}
