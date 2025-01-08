f1cea3a514163d508fdc189a0dc4cbb991ae2a2b
pvdsteen <pvdsteen@gmail.com>
2025-01-08 02:58:55 +0100

    feat: add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+import { page } from "$app/state";
+import Hero from "$lib/components/shell/Hero.svelte";
+</script>
+
+<Hero title={page.status.toString()}>
+	{page?.error?.message}
+</Hero>
