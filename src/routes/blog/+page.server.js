715c1e3cd2b8cb9b4f7282f9d3fad37af3c9340c
pvdsteen <pvdsteen@gmail.com>
2025-01-08 02:55:06 +0100

    refactor: use route id to get page slug

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -3,8 +3,8 @@ import { getPage, getPosts } from "$lib/server/content.js";
 export const prerender = true; // Ensure the site is statically generated
 
 /** @type {import('./$types').PageServerLoad} */
-export const load = async () => {
-	const slug = "blog";
+export const load = async ({ route }) => {
+	const slug = route.id.replace("/", "");
 	const page = getPage(slug);
 	const posts = getPosts();
 
--- src/routes/services/+page.server.js
+++ src/routes/services/+page.server.js
@@ -3,10 +3,10 @@ import { getPage, getServices } from "$lib/server/content.js";
 export const prerender = true; // Ensure the site is statically generated
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load() {
-	const slug = "services";
+export const load = async ({ route }) => {
+	const slug = route.id.replace("/", "");
 	const page = getPage(slug);
 	const services = getServices();
 
 	return { local: { ...page, services } };
-}
+};
