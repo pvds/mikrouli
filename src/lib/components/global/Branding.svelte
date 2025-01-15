39569c258efe4d5087854cde6b3f2b699618417c
pvdsteen <pvdsteen@gmail.com>
2025-01-15 12:00:11 +0100

    fix: tweak icon positioning

--- src/lib/components/global/Branding.svelte
+++ src/lib/components/global/Branding.svelte
@@ -4,7 +4,7 @@ import Logo from "../visuals/Logo.svelte";
 
 <section role="banner">
 	<a class="flex gap-3 items-center" href="./">
-		<Logo classes="fill-primary-200 size-10"/>
+		<Logo classes="relative top-[1px] size-10 fill-primary-200"/>
 		<h1 class="font-medium text-2xl text-primary-50">Mikrouli</h1>
 	</a>
 </section>
