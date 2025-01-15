69680f0b39a16c94e66e8ae06999122e2e335f44
pvdsteen <pvdsteen@gmail.com>
2025-01-15 11:21:37 +0100

    refactor: simplify focus by using native logic instead of js

--- src/lib/components/global/Skip.svelte
+++ src/lib/components/global/Skip.svelte
@@ -1,12 +1,5 @@
-<script>
-/** @type {{focusElement: HTMLElement|undefined}} */
-let { focusElement } = $props();
-const setFocus = () => focusElement?.focus();
-</script>
-
 <a
 	href="#main-content"
 	class="z-10 fixed top-0 left-[50%] -translate-x-[50%] -translate-y-full focus:translate-y-0
 	transform transition-all px-4 py-2 text-center text-lg font-semibold rounded-t-none bg-primary-700 text-white"
-	onclick={setFocus}
 >Skip to content</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,25 +9,19 @@ import Seo from "$lib/components/util/seo/Seo.svelte";
 
 let { children, data } = $props();
 let { nav } = data.local;
-
-/** pointer for skip link
- * @type {HTMLElement|undefined} */
-let mainContent = $state();
 </script>
 
 <Seo/>
 
 <div class="relative overflow-clip flex flex-col app min-h-svh bg-primary-50 text-primary-800">
-	<Skip focusElement={mainContent} />
+	<Skip />
 
 	<Header>
 		<Branding />
 		<NavPrimary menu={nav.primary}/>
 	</Header>
 
-	<main class="grow">
-		<a bind:this={mainContent} id="main-content" href="#main-content" class="sr-only"
-		   tabindex="-1">Main content</a>
+	<main id="main-content" class="grow" tabindex="-1">
 		{@render children()}
 	</main>
 
