697eea8a632f18dfc6a4073d46c6b14df23fae80
pvdsteen <pvdsteen@gmail.com>
2025-01-15 10:49:30 +0100

    feat: add skip to content link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,21 +4,30 @@ import Footer from "$lib/components/global/Footer.svelte";
 import Header from "$lib/components/global/Header.svelte";
 import "../app.css";
 import NavPrimary from "$lib/components/global/NavPrimary.svelte";
+import Skip from "$lib/components/global/Skip.svelte";
 import Seo from "$lib/components/util/seo/Seo.svelte";
 
 let { children, data } = $props();
 let { nav } = data.local;
+
+/** pointer for skip link
+ * @type {HTMLElement|undefined} */
+let mainContent = $state();
 </script>
 
 <Seo/>
 
 <div class="relative overflow-clip flex flex-col app min-h-svh bg-primary-50 text-primary-800">
+	<Skip focusElement={mainContent} />
+
 	<Header>
 		<Branding />
 		<NavPrimary menu={nav.primary}/>
 	</Header>
 
 	<main class="grow">
+		<a bind:this={mainContent} id="main-content" href="#main-content" class="sr-only"
+		   tabindex="-1">Main content</a>
 		{@render children()}
 	</main>
 
