248f4cb0814ff8935f4f342029864fd5a3acf5d6
pvdsteen <pvdsteen@gmail.com>
2025-01-17 11:31:11 +0100

    feat: add processSync util
    Allow easy chained processing

--- src/lib/server/utils.js
+++ src/lib/server/utils.js
@@ -3,6 +3,27 @@ import { marked } from "marked";
 import { gfmHeadingId } from "marked-gfm-heading-id";
 
 /**
+ * Compose multiple functions into a single function.
+ *
+ * First argument is used as the input.
+ * Each function takes the output of the previous function.
+ * Use currying to process multiple arguments.
+ *
+ * @template T - The type of the input
+ * @param {...(arg: T) => T} functions - The functions to compose, each taking the output of the previous function.
+ * @return {(input: T) => T} - The composed function.
+ * @example
+ * const foo = (i) => i + 1;
+ * const bar = (i) => i * 2;
+ * const fooBar = (i, n) => i + n; // Curried function
+ * const addOneThenDouble = compose(foo, bar, (i) => fooBar(i, 1));
+ */
+const processSync =
+	(...functions) =>
+	(input) =>
+		functions.reduce((value, fn) => fn(value), input);
+
+/**
  * Prepend base path to relative links
  * @param {string} content - The content to apply the changes to.
  * @return {string} - The updated content with absolute links.
