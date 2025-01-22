603760f91a0c970427f543129104c54e57eb7a26
pvdsteen <pvdsteen@gmail.com>
2025-01-22 18:17:28 +0100

    chore: remove unused helper

--- src/lib/helpers/conversion.js
+++
@@ -1,7 +0,0 @@
-/**
- * Converts a rem value to pixels
- * @param {string} rem
- * @return {number}
- */
-export const remToPx = (rem) =>
-	Number.parseFloat(rem) * Number.parseFloat(getComputedStyle(document.documentElement).fontSize);
