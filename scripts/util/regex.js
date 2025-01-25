9f0e8bd84175d05fad6d2736fa0c7afec27a0bbb
pvdsteen <pvdsteen@gmail.com>
2025-01-25 03:40:08 +0100

    feat: add escape special regex characters util

---
+++ scripts/util/regex.js
@@ -0,0 +1,7 @@
+/**
+ * Escapes special regex characters in a string.
+ *
+ * @param {string} string - The string to escape.
+ * @returns {string} - The escaped string.
+ */
+export const escapeRegex = (string) => string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
