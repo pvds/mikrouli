19e23ffaa4baacdb4bf45db53dae17d3be2ae550
pvdsteen <pvdsteen@gmail.com>
2025-01-27 22:46:58 +0100

    feat: update service teaser to same format as blog post teaser

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -2,6 +2,8 @@
 import { base } from "$app/paths";
 import Hero from "$layout/Hero.svelte";
 import Section from "$layout/Section.svelte";
+import { getImageName } from "$lib/helpers/image.js";
+import Image from "$ui/image/Image.svelte";
 
 let { data } = $props();
 let { header, intro, slug, services } = data.local;
@@ -24,13 +26,35 @@ const wave = {
 		classes={`px-8 py-14 ${i % 2 === 1 ? "my-14 bg-secondary-100 text-secondary-800" : ""}`}
 		{...(i % 2 === 1 ? { wave } : {})}
 	>
-	<a href={`${base}/${slug}/${service.slug}`}>
-		<div style="view-transition-name:{service.slug}">
-			<h2 class="mb-4 text-3xl font-bold">{service.title}</h2>
-			<div class="prose prose-base">{@html service.intro}</div>
-		</div>
-		<span
-		   class="inline-block mt-4 text-primary-700 hover:underline">Read more →</span>
-	</a>
+		{@render teaser(service, i)}
 	</Section>
 {/each}
+
+
+{#snippet teaser(
+	/** @type {import("$types/contentful").ServiceFields } */ service,
+	/** @type {number} */ i
+)}
+	<a href={`${base}/${slug}/${service.slug}`}
+	   class="group flex flex-wrap items-center gap-4">
+		{#if service.heroImage}
+			<div class="flex-none min-w-[20rem]">
+				<Image
+					image={getImageName(service.heroImage.file.fileName)}
+					aspectRatio={`${service.heroImage.file.details.image.width}/
+					${service.heroImage.file.details.image.height}`}
+					alt={service.heroImage.description}
+					sizes="20rem"
+					priority={(i <= 1)}
+					classes="rounded-md not-group-hover:grayscale"
+					isCMS={true}
+				/>
+			</div>
+		{/if}
+		<div style="view-transition-name: {service.slug}">
+			<h2 class="mb-4 text-3xl font-bold">{service.title}</h2>
+			<div class="prose prose-base">{@html service.intro}</div>
+			<span class="inline-block mt-4 text-primary-700 hover:underline">Read more →</span>
+		</div>
+	</a>
+{/snippet}
