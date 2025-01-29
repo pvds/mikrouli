6c3a5f2ac88e5ac1c88e97d6746c8ffe64a5bea6
pvdsteen <pvdsteen@gmail.com>
2025-01-29 15:20:43 +0100

    feat: make image component more flexible
    - changed: conditional aspect-ratio
    - added: usePlaceholder to allow not rendering placeholder (mainly for transparent images)
    - added: heightClass and widthClass to constrain image

--- src/lib/components/ui/image/Image.svelte
+++ src/lib/components/ui/image/Image.svelte
@@ -3,22 +3,31 @@ import { base } from "$app/paths";
 import { IMAGE_SIZES } from "$config";
 import placeholders from "$data/generated/placeholders.json";
 
-const IMAGE_DIR = "/images";
-const POSITION_CLASSES = "absolute w-full h-full object-center object-cover";
+/**
+ * @typedef {Object} Props
+ * @property {string} image
+ * @property {string} alt
+ * @property {string} sizes
+ * @property {string} [aspectRatio]
+ * @property {string} [heightClass]
+ * @property {string} [widthClass]
+ * @property {boolean} [priority]
+ * @property {boolean} [isCMS]
+ * @property {boolean} [usePlaceholder=true]
+ * @property {string} [classes]
+ */
 
-/** @type {{ image: string, alt: string, sizes: string, aspectRatio?: string, priority?:
- * boolean, isCMS?:
- * boolean,
- * classes?:
- * string }}
- * ImageProps */
+/** @type {Props} */
 let {
 	image,
 	alt,
 	sizes = "(max-width: 768px) 100vw, 50vw",
 	aspectRatio,
+	heightClass,
+	widthClass,
 	priority,
 	isCMS = false,
+	usePlaceholder = true,
 	classes,
 } = $props();
 
@@ -26,13 +35,17 @@ let loaded = $state(true);
 /** @type {HTMLImageElement|undefined} */
 let img = $state(undefined);
 
+const IMAGE_DIR = "/images";
+const POSITION_CLASSES = "absolute object-center object-cover";
+const height = $derived(heightClass ? heightClass : "h-full");
+const width = $derived(widthClass ? widthClass : "w-full");
 const directory = $derived(isCMS ? `${IMAGE_DIR}/cms` : `${IMAGE_DIR}/local`);
 /** @type {Record<string, string>} */
 const cmsPlaceholders = placeholders.cms;
 /** @type {Record<string, string>} */
 const localPlaceholders = placeholders.local;
-/** @type {string} */
-const placeholder = $derived(isCMS ? cmsPlaceholders[image] || "" : localPlaceholders[image] || "");
+/** @type {string|undefined} */
+const placeholder = $derived(isCMS ? cmsPlaceholders[image] : localPlaceholders[image]);
 
 /**
  * @param {number[]} sizes
@@ -44,17 +57,20 @@ const srcset = (sizes) =>
 
 
 {#if loaded}
-<div class="relative"
-	 style={`aspect-ratio: ${aspectRatio}`}
+<div class="relative {height} {width}"
+	 style={aspectRatio && `aspect-ratio: ${aspectRatio}`}
 >
-	<img src={placeholder} {alt} class="{POSITION_CLASSES} {classes}"
+	{#if usePlaceholder && placeholder}
+	<img src={placeholder} {alt}
+		 class="{POSITION_CLASSES} {classes} {height} {width}"
 		 loading={priority ? "eager" : "lazy"}
 		 fetchpriority={priority ? "high" : null}/>
 	<div class="{POSITION_CLASSES} backdrop-blur-xl {classes}"></div>
+	{/if}
 	<picture>
 		<source srcset={srcset(IMAGE_SIZES)} sizes={sizes} type="image/webp" />
 		<img
-			class="{POSITION_CLASSES} transition-all opacity-0 {classes}"
+			class="{POSITION_CLASSES} {classes} {height} {width} transition-all opacity-0"
 			src={`${base}${directory}/${image}-1280.webp`}
 			{alt}
 			bind:this={img}
