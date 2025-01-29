4051ab633f520df9278569a849ed544b500ffe1c
pvdsteen <pvdsteen@gmail.com>
2025-01-29 11:27:11 +0100

    fix: home url

--- src/lib/components/global/Branding.svelte
+++ src/lib/components/global/Branding.svelte
@@ -1,9 +1,10 @@
 <script>
+import { base } from "$app/paths";
 import Logo from "$visuals/Logo.svelte";
 </script>
 
 <section role="banner">
-	<a class="flex gap-3 items-center" href="./">
+	<a class="flex gap-3 items-center" href={base}>
 		<Logo classes="relative top-[1px] size-10 fill-primary-200"/>
 		<h1 class="font-medium text-2xl text-primary-50">Mikrouli</h1>
 	</a>
