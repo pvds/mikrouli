85141b4865ab3df67099531b41e1af4cc9fbacbe
pvdsteen <pvdsteen@gmail.com>
2025-01-31 19:00:25 +0100

    fix: incorrect wave-color for primary theme

--- src/lib/components/layout/Section.svelte
+++ src/lib/components/layout/Section.svelte
@@ -37,7 +37,7 @@ const THEME_CLASSES = {
 	default: "",
 };
 const WAVE_COLORS = {
-	primary: "secondary-lighter",
+	primary: "primary-lighter",
 	secondary: "secondary-lighter",
 	default: "secondary-lighter",
 };
