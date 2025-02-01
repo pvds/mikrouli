cd5a3ac86b3bf6b036f040bd96c68d6f4d90874f
pvdsteen <pvdsteen@gmail.com>
2025-02-01 15:06:39 +0100

    feat: add and implement BlogArticle.svelte component

---
+++ src/lib/components/ui/BlogArticle.svelte
@@ -0,0 +1,38 @@
+<script>
+import { base } from "$app/paths";
+import { getImageName } from "../../helpers/image.js";
+import Image from "./image/Image.svelte";
+
+/**
+ * @typedef {import("$types/contentful").PostFields } PostFields
+ * @typedef {Object} Props
+ * @property {PostFields} post
+ * @property {boolean} [priority=false]
+ */
+
+/** @type {Props} */
+let { post, priority = false } = $props();
+</script>
+<article
+	class="group relative flex flex-wrap items-center gap-4">
+	{#if post.heroImage}
+		<div class="flex-none min-w-[20rem]">
+			<Image
+				image={getImageName(post.heroImage.file.fileName)}
+				alt={post.heroImage.description}
+				sizes="20rem"
+				{priority}
+				classes="rounded-md not-group-hover:grayscale"
+				isCMS={true}
+			/>
+		</div>
+	{/if}
+	<div style="view-transition-name: {post.slug}">
+		<h2 class="mb-4 text-3xl font-bold">{post.title}</h2>
+		<div class="prose prose-base">{@html post.intro}</div>
+		<a href={`${base}/blog/${post.slug}`}
+		   class="inline-block mt-4 font-semibold transition-all hover:underline group-hover:text-accent-dark after:content[''] after:absolute after:inset-0">Read more
+			<span class="inline-block group-hover:animate-wiggle-right">→</span>
+		</a>
+	</div>
+</article>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,13 +1,10 @@
 <script>
-import { base } from "$app/paths";
-import ContentSection from "$layout/ContentSection.svelte";
 import Hero from "$layout/Hero.svelte";
 import Section from "$layout/Section.svelte";
-import { getImageName } from "$lib/helpers/image";
-import Image from "$ui/image/Image.svelte";
+import BlogArticle from "$ui/BlogArticle.svelte";
 
 let { data } = $props();
-let { header, intro, slug, sections } = data.page.fields; // Destructure `posts` from `local`
+let { header, intro } = data.page.fields;
 let posts = data.posts;
 </script>
 
@@ -24,35 +21,6 @@ let posts = data.posts;
 
 {#each posts as post, i}
 	<Section wave={i % 2 === 1}>
-		{@render teaser(post.fields, i)}
+		<BlogArticle post={post.fields} priority={i <= 3}/>
 	</Section>
 {/each}
-
-{#snippet teaser(
-	/** @type {import("$types/contentful").PostFields } */ post,
-	/** @type {number} */ i
-)}
-	<article
-	   class="group relative flex flex-wrap items-center gap-4">
-		{#if post.heroImage}
-			<div class="flex-none min-w-[20rem]">
-				<Image
-					image={getImageName(post.heroImage.file.fileName)}
-					alt={post.heroImage.description}
-					sizes="20rem"
-					priority={(i <= 1)}
-					classes="rounded-md not-group-hover:grayscale"
-					isCMS={true}
-				/>
-			</div>
-		{/if}
-		<div style="view-transition-name: {post.slug}">
-			<h2 class="mb-4 text-3xl font-bold">{post.title}</h2>
-			<div class="prose prose-base">{@html post.intro}</div>
-			<a href={`${base}/${slug}/${post.slug}`}
-				class="inline-block mt-4 font-semibold transition-all hover:underline group-hover:text-accent-dark after:content[''] after:absolute after:inset-0">Read more
-				<span class="inline-block group-hover:animate-wiggle-right">→</span>
-			</a>
-		</div>
-	</article>
-{/snippet}
