4e4bcc3fb41bae91365f24fe8ff9c9b2cb9ab313
pvdsteen <pvdsteen@gmail.com>
2025-02-01 14:46:01 +0100

    feat: add title to ServicesSection.svelte

--- src/lib/components/layout/ServicesSection.svelte
+++ src/lib/components/layout/ServicesSection.svelte
@@ -12,11 +12,13 @@ import Section from "./Section.svelte";
 let { services } = $props();
 </script>
 
-<Section innerClasses="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-12">
+<Section title="My Services">
+	<div class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-12">
 	{#each services as service, i}
 		<div class="col-span-2 {oddLastEntry(services.length, i) &&
 		'sm:col-start-2 lg:col-start-auto'}">
 			<ServiceArticle service={service.fields} priority={i <= 3}/>
 		</div>
 	{/each}
+	</div>
 </Section>
