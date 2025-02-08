f1aac0bfde7b4371549109bc278d8715e6edfa3d
pvdsteen <pvdsteen@gmail.com>
2025-02-08 12:46:20 +0100

    sec: add minimal CSP
    Need to figure out how to get and use dynamically generated hashes

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,27 @@ import adapterStatic from "@sveltejs/adapter-static";
 const target = process.env.DEPLOY_TARGET || "staging";
 const production = target === "production";
 
+const CSP_YOUTUBE = "https://www.youtube.com";
+const CSP_SETMORE = "https://mikrouli.setmore.com";
+const CSP_WHITELIST = {
+	frames: [CSP_YOUTUBE, CSP_SETMORE],
+	media: [CSP_YOUTUBE], // Ensures video playback works
+};
+const CSP_REPORT_URI = production ? "/" : "/mikrouli";
+/** @type {import('@sveltejs/kit').CspDirectives} */
+const CSP = {
+	"default-src": ["self"], // Default policy for loading resources
+	"script-src": ["self", "unsafe-inline"], // No external scripts
+	"style-src": ["self", "unsafe-inline"], // No external styles
+	"img-src": ["self", "data:"], // No external images
+	"connect-src": ["self"], // No external fetch requests
+	"font-src": ["self"], // No external fonts
+	"object-src": ["none"], // Blocks Flash, ActiveX, etc.
+	"frame-src": ["self", ...CSP_WHITELIST.frames],
+	"media-src": ["self", ...CSP_WHITELIST.media],
+	"frame-ancestors": ["none"], // Prevents embedding in iframes
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -35,6 +56,10 @@ const config = {
 		},
 		serviceWorker: {
 			register: false,
+		},
+		csp: {
+			directives: { ...CSP },
+			reportOnly: { ...CSP, "report-uri": [CSP_REPORT_URI] },
 		},
 	},
 };
