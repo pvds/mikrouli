4a7c3b815681344e69e7eb0910c06b109e8991da
pvdsteen <pvdsteen@gmail.com>
2025-02-09 12:12:35 +0100

    fix: harden getImageName method

--- src/lib/helpers/image.js
+++ src/lib/helpers/image.js
@@ -3,4 +3,4 @@
  * @param {string} fileName - The image file name like "image.jpg"
  * @return {string} - The image name without the extension like "image"
  */
-export const getImageName = (fileName) => fileName.split(".")[0];
+export const getImageName = (fileName) => fileName.split(".").slice(0, -1).join(".");
