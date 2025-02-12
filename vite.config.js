71290eb943238d85e9f0ff67ec2a5a61ed1cc40e
pvdsteen <pvdsteen@gmail.com>
2025-02-12 14:34:39 +0100

    feat: add browser-sync

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import VitePluginBrowserSync from "vite-plugin-browser-sync";
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
+	plugins: [VitePluginBrowserSync(), sveltekit(), tailwindcss()],
 });
