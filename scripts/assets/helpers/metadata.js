ad681630abe3d608fdbace3503661b771270d2c6
pvdsteen <pvdsteen@gmail.com>
2025-02-14 02:05:27 +0100

    perf: prefer bigger component bundle over rendering delay and many network requests

--- scripts/assets/helpers/metadata.js
+++ scripts/assets/helpers/metadata.js
@@ -1,26 +1,38 @@
 import path from "node:path";
 import { IMAGES_METADATA_OUTPUT_PATH_RESOLVED } from "$util/dyn.js";
-import { prepareDir, writeJSON } from "$util/file.js";
+import { prepareDir, readJSON, writeJSON } from "$util/file.js";
 import { logInfo, logSuccess } from "$util/log.js";
 
 /**
+ * @typedef {import('$types/content').ImageMeta} ImageMeta
+ * @typedef {Record<string, ImageMeta>} Metadata
+ *
  * Writes metadata for each image as a separate JSON file.
  * @param {string} category - The category (e.g., "cms" or "local").
- * @param {Record<string, { placeholder: string, width: string, height: string, hasAlpha: boolean }>} metadata - The image metadata.
+ * @param {Metadata} metadata - The image metadata.
  */
 export const writeMetadata = async (category, metadata) => {
 	logInfo(`\nWriting ${category} images metadata...`);
 
-	const outputDir = path.join(IMAGES_METADATA_OUTPUT_PATH_RESOLVED, category);
-	prepareDir(outputDir, true);
+	const outputPath = path.join(IMAGES_METADATA_OUTPUT_PATH_RESOLVED, category);
+	const outputMetadataPath = path.join(IMAGES_METADATA_OUTPUT_PATH_RESOLVED, "images.json");
+	prepareDir(outputPath, true);
+
+	const metadataFileContents = /** @type {Record<string, Metadata>} */ (
+		readJSON(outputMetadataPath)
+	);
+	metadataFileContents[category] = Object.fromEntries(Object.entries(metadata).sort());
 
 	// Write each image metadata to its own file
 	await Promise.all(
 		Object.entries(metadata).map(async ([imageName, meta]) => {
-			const filePath = path.join(outputDir, `${imageName}.json`);
+			const filePath = path.join(outputPath, `${imageName}.json`);
 			await writeJSON(filePath, meta);
 		}),
 	);
 
-	logSuccess(`Wrote ${category} images metadata to separate files`);
+	// Sort placeholders alphabetically and assign to the category
+	writeJSON(outputMetadataPath, metadataFileContents);
+
+	logSuccess(`Wrote ${category} images metadata`);
 };
