2b8e1ac12b75b249b52cc797a5cef7e155fa4bc4
pvdsteen <pvdsteen@gmail.com>
2025-09-12 07:10:34 +0200

    refactor: use full jsdoc comment
    ensures type safety

--- scripts/util/error.js
+++ scripts/util/error.js
@@ -1,2 +1,6 @@
-/** @type {(e: unknown) => string} */
+/**
+ * Extract error message from an unknown error object.
+ * @param {unknown} e
+ * @return {string}
+ */
 export const errMsg = (e) => (e instanceof Error ? e.message : String(e));
